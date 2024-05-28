4196f5bdbbcd96d7a63c9f67d475c127634fa308
Ahmed Khalifa <ahmedkhalifa201395@yahoo.com>
2024-05-28 20:46:33 +0300

    Add redux store to counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,28 @@
-import classes from './Counter.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+
+  const incrementHnadler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHnadler = () => {
+    dispatch({ type: "decrement" });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHnadler}>Increment</button>
+        <button onClick={decrementHnadler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
